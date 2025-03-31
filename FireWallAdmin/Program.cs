a9d973852aafb48bdec14f7f6dcbf819c0f99f31
zmrbak <20995658@qq.com>
2025-03-31 09:27:26 +0800

    删除多策略设置，只设置一个策略

--- FireWallAdmin/Program.cs
+++ FireWallAdmin/Program.cs
@@ -23,7 +23,7 @@ namespace FireWallAdmin
         {
             if (ConfigFileCheck() == false) return;
             string jsonRules = File.ReadAllText(fireWallConfigFile);
-            List<NetFwRule> netFwRules = (List<NetFwRule>)(new JavaScriptSerializer().Deserialize(jsonRules, typeof(List<NetFwRule>)));
+            NetFwRule netFwRules = new JavaScriptSerializer().Deserialize<NetFwRule>(jsonRules);
 
             //实例化COM引用对象
             Type type = Type.GetTypeFromProgID("HNetCfg.FwPolicy2");
@@ -34,33 +34,30 @@ namespace FireWallAdmin
             INetFwPolicy.FirewallEnabled[NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_PRIVATE] = true;
             INetFwPolicy.FirewallEnabled[NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_DOMAIN] = true;
 
+            //删除旧策略
+            INetFwPolicy.Rules.Remove(netFwRules.Name);
             //添加策略
-            foreach (NetFwRule item in netFwRules)
-            {
-                //删除旧策略
-                INetFwPolicy.Rules.Remove(item.Name);
+            NetFwRule item = netFwRules;
+            //添加新策略
+            INetFwRule INetFwRule = (INetFwRule)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwRule"));
+            INetFwRule.Name = item.Name;
+            INetFwRule.Description = item.Description;
+            INetFwRule.Enabled = item.Enabled;
+            INetFwRule.Action = item.Action;
+            INetFwRule.Direction = item.Direction;
+            INetFwRule.LocalAddresses = item.LocalAddresses;
+            INetFwRule.Protocol = item.Protocol;
 
-                //添加新策略
-                INetFwRule INetFwRule = (INetFwRule)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwRule"));
-                INetFwRule.Name = item.Name;
-                INetFwRule.Description = item.Description;
-                INetFwRule.Enabled = item.Enabled;
-                INetFwRule.Action = item.Action;
-                INetFwRule.Direction = item.Direction;
-                INetFwRule.LocalAddresses = item.LocalAddresses;
-                INetFwRule.Protocol = item.Protocol;
+            string localPorts = string.Join(",", item.LocalPorts.Select(x => x.PortNumber).OrderBy(x => x));
+            INetFwRule.LocalPorts = localPorts;
+            INetFwRule.InterfaceTypes = item.InterfaceTypes;
 
-                string localPorts = string.Join(",", item.LocalPorts.Select(x => x.PortNumber).OrderBy(x=>x));
-                INetFwRule.LocalPorts = localPorts;
-                INetFwRule.InterfaceTypes = item.InterfaceTypes;
+            string remoteAddresses = string.Join(",", item.RemoteAddresses.Select(x => x.IpAddress).OrderBy(x => x));
+            INetFwRule.RemoteAddresses = remoteAddresses.Replace(" ", "").Replace(",,", ",").Replace(",,", ",");
+            INetFwRule.RemotePorts = item.RemotePorts;
+            INetFwPolicy.Rules.Add(INetFwRule);
 
-                string remoteAddresses = string.Join(",", item.RemoteAddresses.Select(x => x.IpAddress).OrderBy(x => x));
-                INetFwRule.RemoteAddresses = remoteAddresses.Replace(" ","").Replace(",,",",").Replace(",,", ",");
-                INetFwRule.RemotePorts = item.RemotePorts;
-                INetFwPolicy.Rules.Add(INetFwRule);
-
-                Console.WriteLine("添加规则：" + item.Name + "成功！");
-            }
+            Console.WriteLine("添加规则：" + item.Name + "成功！");
 
             //打开防火墙管理界面
             Process.Start("mmc.exe", "wf.msc");
@@ -82,7 +79,6 @@ namespace FireWallAdmin
                 configSample.AppendLine("配置文件名：\t" + fireWallConfigFile);
                 configSample.AppendLine("文件内容示例:");
 
-                List<NetFwRule> netFwRules = new List<NetFwRule>();
                 NetFwRule netFwRule = new NetFwRule();
                 netFwRule.Name = "测试规则";
                 netFwRule.Description = "这是一个测试规则";
@@ -110,10 +106,7 @@ namespace FireWallAdmin
 
                 //默认
                 //netFwRule.RemotePorts = "*";
-
-                netFwRules.Add(netFwRule);
-
-                string jsonSample = new JavaScriptSerializer().Serialize(netFwRules);
+                string jsonSample = new JavaScriptSerializer().Serialize(netFwRule);
                 configSample.Append(jsonSample);
                 Console.WriteLine(configSample.ToString());
 
